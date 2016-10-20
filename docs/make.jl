90cdbd14ea1f21f6125bf015d5f307a63d7e4957
Pranav <pranavtbhat@gmail.com>
2016-10-20 13:42:42 -0700

    Add cinder dep

--- docs/make.jl
+++ docs/make.jl
@@ -2,7 +2,7 @@ using Documenter
 
 makedocs()
 
-custom_deps() = run(`pip install --user pygments mkdocs mkdocs-material`)
+custom_deps() = run(`pip install --user pygments mkdocs mkdocs-material mkdocs-cinder`)
 
 deploydocs(
    #options

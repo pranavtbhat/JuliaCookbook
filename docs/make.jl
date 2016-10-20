fbf40cf68cca2f57e14bd200840edc03e2dfae1a
Pranav <pranavtbhat@gmail.com>
2016-10-20 13:50:39 -0700

    Fix deps again :'(

--- docs/make.jl
+++ docs/make.jl
@@ -4,7 +4,7 @@ makedocs()
 
 deploydocs(
    #options
-   deps = Deps.pip("pygments", "mkdocs", "python-markdown-math", "mkdocs"),
+   deps = Deps.pip("pygments", "mkdocs", "python-markdown-math", "mkdocs-cinder"),
    repo = "github.com/pranavtbhat/JuliaCookbook.git",
    julia = "0.5"
 )

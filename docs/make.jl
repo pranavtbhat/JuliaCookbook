4691ec754e28911371c211ecc8c0b03d6c75116c
Pranav <pranavtbhat@gmail.com>
2016-10-20 13:47:36 -0700

    Fix deps again :'(

--- docs/make.jl
+++ docs/make.jl
@@ -2,11 +2,9 @@ using Documenter
 
 makedocs()
 
-custom_deps() = run(`pip install --user pygments mkdocs mkdocs-material mkdocs-cinder`)
-
 deploydocs(
    #options
-   deps = custom_deps,
+   deps = Deps.pip("pygments", "mkdocs", "python-markdown-math", "mkdocs"),
    repo = "github.com/pranavtbhat/JuliaCookbook.git",
    julia = "0.5"
 )
